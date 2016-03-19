a8f34a74f4ff6226128a5f489a0f81e6057ea031
cxnthu <2024281531@qq.com>
2016-03-19 11:11:48 +0800

    move files

---
+++ ai/analyzer.cpp
@@ -0,0 +1,18 @@
+#include "../headers/common.h"
+#include "../headers/teamstyle17.h"
+
+const Map *map=GetMap();
+
+Position* pos(ObjectType A)
+{
+	int number;
+	number=map->objects_number;
+}
+
+Position cloest(ObjectType A)
+{
+
+}
+
+
+
