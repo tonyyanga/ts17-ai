f28d0c39206e56e76b592aab9fc1fead8082b692
Tsre <songsirui97328@gmail.com>
2016-03-19 23:12:02 +0800

    add some functions

--- headers/common.h
+++ headers/common.h
@@ -1,3 +1,4 @@
 #include "basic.h"
 
 int usetime(bool add,Position &p,Position &p0,PlayerObject &Self);	//不计算速度方向改变的回合时,返回从p到p0的回合数,add为假则代表不使用加速技能
+double angle(Position p1,Position p2);		//计算p1,p2矢量夹角（返回arccos值）
